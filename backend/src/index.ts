23ea920c9b46baccef71005443096f73748e102a
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-09-30 20:35:49 +0800

    chore(backend): allow CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3050);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hello Elysia").listen(3050);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`

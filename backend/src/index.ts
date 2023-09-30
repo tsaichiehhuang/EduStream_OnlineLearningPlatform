91a3ea61cde488eb72f6ecd5c44f78c54a984847
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-09-30 20:44:55 +0800

    feat: simple example to use API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { cors } from '@elysiajs/cors';
 
 const app = new Elysia()
   .use(cors())
-  .get("/", () => "Hello Elysia").listen(3050);
+  .get("/", () => "Hello World!")
+  .post("/hello", ({ body }) => `Hello ${body.name}`, {
+    body: t.Object({
+      name: t.String(),
+    })
+  })
+  .listen(3050);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+export type App = typeof app;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,29 @@
+'use client'
+import { edenTreaty } from '@elysiajs/eden';
+import type { App } from "@/../../backend/src/index";
+
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react';
+
+const api = edenTreaty<App>('http://localhost:3050');
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [greet, setGreet] = useState("");
+
+  function sendApi() {
+    api.hello.post({
+      name: name
+    }).then(response => {
+      if (response.error) {
+        setGreet("發生錯誤😭");
+      } else {
+        setGreet(response.data);
+      }
+    });
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -9,86 +31,16 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div>
+        <input
+          type='text'
+          value={name}
+          onChange={event => { setName(event.target.value); }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <button onClick={sendApi}>讓伺服器跟你打招呼!</button>
+        <div>{greet}</div>
       </div>
     </main>
   )

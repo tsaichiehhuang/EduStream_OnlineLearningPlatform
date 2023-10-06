837c0b27cd98723a0d7d0ebd8b81a6a86219ffab
tsaichiehhuang <a0970605512@gmail.com>
2023-10-06 15:39:05 +0800

    frontend init

--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Home from '@/pages/home'
 
-export default function Index() {
-    return <Home />
+export default function Signup() {
+    return <></>
 }

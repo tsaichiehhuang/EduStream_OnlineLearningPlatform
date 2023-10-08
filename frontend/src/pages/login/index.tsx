537867888de9224837b2089ab6bb7e87fd8b669e
penny <penny2014928@gmail.com>
2023-10-08 23:21:10 +0800

    feat：login & signup page

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -2,6 +2,162 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
+import Header from '@/components/header'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Link from 'next/link';
+
 export default function Login() {
-    return <div>你好嗎 hello</div>
+
+  const [logining, setlogininging] = useState(false);
+
+
+  
+  const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
+  const loginValidationSchema = Yup.object().shape({
+    email1: Yup.string().required('Email is required').email('Invalid email address'),
+    password1: Yup.string()
+      .required('Password is required')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+        'Password must contain uppercase letter, lowercase letter, and number'
+      )
+  });
+
+
+  const formikLogin = useFormik({
+    initialValues: {
+      email1: '',
+      password1: ''
+    },
+    validationSchema: loginValidationSchema,
+    onSubmit: async (values) => {
+      const { email1, password1 } = values;
+      setlogininging(true);
+
+      try {
+        const loginResponse = await fetch(
+          'https://penny-canchu-api.octave.vip/api/1.0/users/signin',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              provider: 'native',
+              email: String(email1),
+              password: String(password1)
+            })
+          }
+        );
+
+        const loginData = await loginResponse.json();
+        console.log(loginData);
+
+        if (loginResponse.ok) {
+          console.log('登入成功');
+          document.cookie = `token=${loginData.data.access_token}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+          document.cookie = `userId=${loginData.data.user.id}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+          window.location.href = '/';
+        } else {
+          console.error('登入失敗:', loginData.error);
+
+          if (loginResponse.status >= 500 && loginResponse.status <= 599) {
+            alert('出現錯誤。請稍後再試或通知我們的工程團隊。');
+            window.location.href = '/login';
+          } else {
+            const errorMessage = `登入失敗: ${loginData.error}`;
+            alert(errorMessage);
+            window.location.href = '/login';
+          }
+        }
+      } catch (error) {
+        console.error('請求錯誤:', error);
+      }
+    }
+  });
+
+  
+
+
+  return (
+    <><div className="flex flex-col  justify-center mt-5 items-center bg-white">
+          <div className="bg-white rounded-lg mt h-[55%] w-[60%]">
+              <div className="flex flex-row">
+                 
+                  <div className="w-[65%] bg-white p-10 rounded-r-lg">
+           
+                  <div className="light dark:text-white text-mainBlue text-center font-zen-dots text-4xl font-normal" >
+                        EduStream
+                    </div>
+                      <div className="text-2xl text-center my-10">會員登入</div>
+                     
+                          <form onSubmit={formikLogin.handleSubmit} className="w-4/5 mx-auto">
+                              <div className="mb-5">
+                                  <label htmlFor="email1" className="block font-medium text-lg text-gray-700">電子郵件</label>
+                                  <input
+                                      type="email"
+                                      id="email1"
+                                     
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      placeholder="例: shirney@appworks.tw"
+                                      {...formikLogin.getFieldProps('email1')} />
+                                  {formikLogin.touched.email1 && formikLogin.errors.email1 && (
+                                      <div className="text-red-500 mt-1">{formikLogin.errors.email1}</div>
+                                  )}
+                              </div>
+
+                              <div className="mb-5">
+                                  <label htmlFor="password1" className="block font-medium text-lg text-gray-700">密碼</label>
+                                  <input
+                                      type="password"
+                                      id="password1"
+                                     
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      {...formikLogin.getFieldProps('password1')} />
+                                  {formikLogin.touched.password1 && formikLogin.errors.password1 && (
+                                      <div className="text-red-500 mt-1">{formikLogin.errors.password1}</div>
+                                  )}
+                              </div>
+                              {logining ? (
+                                  <div className="mb-5">
+                                      <div className="bg-gray-500 w-full h-12 rounded flex items-center justify-center">
+                                          <div className="text-white">登入中...</div>
+                                      </div>
+                                  </div>
+                              ) : (
+                                  <div className="mb-5">
+                                      <button className="bg-mainBlue w-full h-12 rounded text-white font-medium text-lg hover:bg-blue-400" type="submit">
+                                          登入
+                                      </button>
+                                  </div>
+                              )}
+                          </form>
+                     
+                      <div className="text-center mt-3">
+                        
+                              <div className="text-gray-700">
+                                  尚未成為會員?{' '}
+                                 
+                                  <Link className="text-mainBlue"  href="/signup">
+                                      會員註冊
+                                  </Link>
+                              </div>
+                        
+                      </div>
+                  </div>
+                  <div className="w-[35%] bg-mainBlue rounded-r-lg">
+                      
+                  </div>
+              </div>
+          </div>
+         
+      </div></>
+  );
 }
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -2,6 +2,189 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
-export default function Signup() {
-    return <div>你好嗎 hello</div>
+import Header from '@/components/header'
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Link from 'next/link';
+
+export default function Login() {
+  const [isregister, setregister] = useState(false);
+  const [signuping, setsignuping] = useState(false);
+
+  
+  const [theme, setTheme] = useState('light')
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
+  
+
+  const registerValidationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().required('Email is required').email('Invalid email address'),
+    password: Yup.string()
+      .required('Password is required')
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+        'Password must contain uppercase letter, lowercase letter, and number'
+      ),
+    repassword: Yup.string()
+      .required('Please confirm your password')
+      .oneOf([Yup.ref('password'), "null"], 'Passwords must match')
+  });
+
+
+  const formikRegister = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      repassword: ''
+    },
+    validationSchema: registerValidationSchema,
+    onSubmit: async (values) => {
+      setsignuping(true);
+      const { name, email, password } = values;
+      console.log(name);
+      console.log(email);
+      console.log(password);
+
+      const requestData = {
+        name: String(name),
+        email: String(email),
+        password: String(password)
+      };
+
+      try {
+        const response = await fetch('https://penny-canchu-api.octave.vip/api/1.0/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(requestData)
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+          console.log('註冊成功');
+          setregister(!isregister);
+          setsignuping(false);
+        
+        } else {
+          console.error('註冊失敗:', data.error);
+          if (response.status >= 500 && response.status <= 599) {
+            alert('出現錯誤。請稍後再試或通知我們的工程團隊。');
+            window.location.href = '/login';
+          } else {
+            const errorMessage = `註冊失敗: ${data.error}`;
+            alert(errorMessage);
+            window.location.href = '/login';
+          }
+        }
+      } catch (error) {
+        console.error('請求錯誤:', error);
+      }
+    }
+  });
+
+  return (
+    <><div className="flex flex-col items-center bg-white">
+          <div className="bg-white rounded-lg mt-5 w-[60%]">
+              <div className="flex flex-row">
+                 
+                  <div className="w-[65%] bg-white p-10 rounded-r-lg">
+           
+                  <div className="light dark:text-white text-mainBlue text-center font-zen-dots text-4xl font-normal" >
+                        EduStream
+                    </div>
+                      <div className="text-2xl text-center my-10">會員註冊</div>
+                     
+                          <form onSubmit={formikRegister.handleSubmit} className="w-4/5 mx-auto">
+                              <div className="mb-5">
+                                  <label htmlFor="name" className="block font-medium text-lg text-gray-700">使用者名稱</label>
+                                  <input
+                                      type="text"
+                                      id="name"
+                                   
+                                      placeholder="例: Chou Chou Hu"
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      {...formikRegister.getFieldProps('name')} />
+                                  {formikRegister.touched.name && formikRegister.errors.name && (
+                                      <div className="text-red-500 mt-1">{formikRegister.errors.name}</div>
+                                  )}
+                              </div>
+                              <div className="mb-5">
+                                  <label htmlFor="email" className="block font-medium text-lg text-gray-700">電子郵件</label>
+                                  <input
+                                      type="email"
+                                      id="email"
+                                  
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      placeholder="例: shirney@appworks.tw"
+                                      {...formikRegister.getFieldProps('email')} />
+                                  {formikRegister.touched.email && formikRegister.errors.email && (
+                                      <div className="text-red-500 mt-1">{formikRegister.errors.email}</div>
+                                  )}
+                              </div>
+                              <div className="mb-5">
+                                  <label htmlFor="password" className="block font-medium text-lg text-gray-700">密碼</label>
+                                  <input
+                                      type="password"
+                                      id="password"
+                               
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      {...formikRegister.getFieldProps('password')} />
+                                  {formikRegister.touched.password && formikRegister.errors.password && (
+                                      <div className="text-red-500 mt-1">{formikRegister.errors.password}</div>
+                                  )}
+                              </div>
+                              <div className="mb-5">
+                                  <label htmlFor="repassword" className="block font-medium text-lg text-gray-700">確認密碼</label>
+                                  <input
+                                      type="password"
+                                      id="repassword"
+                                  
+                                      className="w-full h-12 px-3 rounded border  focus:outline-none focus:border-black"
+                                      {...formikRegister.getFieldProps('repassword')} />
+                                  {formikRegister.touched.repassword && formikRegister.errors.repassword && (
+                                      <div className="text-red-500 mt-1">{formikRegister.errors.repassword}</div>
+                                  )}
+                              </div>
+                              {signuping ? (
+                                  <div className="mb-5">
+                                      <div className="bg-gray-500 w-full h-12 rounded flex items-center justify-center">
+                                          <div className="text-white">註冊中...</div>
+                                      </div>
+                                  </div>
+                              ) : (
+                                  <div className="mb-5">
+                                      <button className="bg-mainBlue w-full h-12 rounded text-white font-medium text-lg hover:bg-blue-400" type="submit">
+                                          註冊
+                                      </button>
+                                  </div>
+                              )}
+                          </form>
+                    
+                      <div className="text-center mt-3">
+                    
+                              <div className="text-gray-700">
+                                  已經是會員了?{' '}
+                                  <Link className="text-mainBlue"  href="/login">
+                                      會員登入
+                                  </Link>
+                              </div>
+                      
+                             
+                      </div>
+                  </div>
+                  <div className="w-[35%] bg-mainBlue text-white p-10 rounded-r-lg">
+                      
+                  </div>
+              </div>
+          </div>
+      </div></>
+  );
 }

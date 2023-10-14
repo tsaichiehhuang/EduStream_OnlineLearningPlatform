6187ffc4c03deaa5e4a253aa955598eb4442cbe8
Distant22 <sindistant@gmail.com>
2023-10-14 16:22:04 +0800

    feat(routes):add live routes

--- backend/src/routes/live_route.ts
+++ backend/src/routes/live_route.ts
@@ -1,6 +1,18 @@
 import { Elysia, t } from "elysia";
-import { createLive } from "../live/live_controller";
+import { createLive } from "../live/create";
+import { cancelLive } from "../live/cancel";
+import { getLive } from "../live/get";
+import { endLive } from "../live/end";
+import { startLive } from "../live/start";
+import { previewLive } from "../live/preview";
+import { archiveLive } from "../live/archieve";
 
-export const liveRoute = new Elysia().group("live", (app) =>
-  app.use(createLive())
+export const liveRoute = new Elysia().group("live", (app) => app
+  .use(getLive())
+  .use(endLive())
+  .use(startLive())
+  .use(createLive())
+  .use(cancelLive())
+  .use(previewLive())
+  .use(archiveLive())
 );

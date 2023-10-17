d38c842f01280a4e70e577c0720019a5e97aa0f3
Hsin-Yun Lai <lai1029700@gmail.com>
2023-10-17 09:46:04 +0800

    refactor: auth

--- backend/src/user/info.ts
+++ backend/src/user/info.ts
@@ -1,41 +1,42 @@
-// get user info api
 import { Elysia, t } from "elysia";
-import { bearer } from "@elysiajs/bearer";
-import { jwt } from "@elysiajs/jwt";
+// import { bearer } from "@elysiajs/bearer";
+// import { jwt } from "@elysiajs/jwt";
 import { IUser, UserRole } from "../types/type";
 import { User } from "../models/user";
+import { auth } from "../utils/auth";
 
 export const info = (app: Elysia) =>
   app
-    .use(
-      jwt({
-        name: "jwt",
-        secret: process.env.JWT_SECRET ? process.env.JWT_SECRET : "",
-      })
-    )
-    .use(bearer())
-    .derive(async ({ jwt, bearer }) => {
-      const profile = await jwt.verify(bearer);
+    // .use(
+    //   jwt({
+    //     name: "jwt",
+    //     secret: process.env.JWT_SECRET ? process.env.JWT_SECRET : "",
+    //   })
+    // )
+    // .use(bearer())
+    // .derive(async ({ jwt, bearer }) => {
+    //   const profile = await jwt.verify(bearer);
 
-      return {
-        profile: profile,
-      };
-    })
+    //   return {
+    //     profile: profile,
+    //   };
+    // })
+    .use(auth)
     .get(
       "/info/:id",
       async ({ profile, set, params: { id } }) => {
-        if (!profile) {
-          set.status = 401;
-          return "Unauthorized";
-        }
+        // if (!profile) {
+        //   set.status = 401;
+        //   return "Unauthorized";
+        // }
+        if (set.status !== 200 || !profile) return "Unauthorized";
 
-        if (Number(profile.id) !== Number(id)) {
+        if (Number(profile.id) !== id) {
           set.status = 403;
           return "Permission Denied";
         }
 
-        const user = await User.findOneBy({ id: Number(id) });
-
+        const user = await User.findOneBy({ id: id });
         if (!user) {
           set.status = 404;
           return "User Not Found";
@@ -55,18 +56,18 @@ export const info = (app: Elysia) =>
         };
       },
       {
-        async beforeHandle({ bearer, set }) {
-          if (!bearer) {
-            set.status = 401;
-            set.headers[
-              "WWW-Authenticate"
-            ] = `Bearer realm='sign', error="invalid_request"`;
+        // async beforeHandle({ set }) {
+        //   if (!bearer) {
+        //     set.status = 401;
+        //     set.headers[
+        //       "WWW-Authenticate"
+        //     ] = `Bearer realm='sign', error="invalid_request"`;
 
-            return "Error: Unauthorized";
-          }
-        },
+        //     return "Error: Unauthorized";
+        //   }
+        // },
         params: t.Object({
-          id: t.String(),
+          id: t.Numeric(),
         }),
       }
     );

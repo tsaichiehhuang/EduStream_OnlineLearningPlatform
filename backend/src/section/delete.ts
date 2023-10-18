d0998ab844dc089ec4d2243ee0e3f7e34bf73b12
lai10297000 <lai10297000@users.noreply.github.com>
2023-10-18 16:29:26 +0800

    chore(Prettier): Prettified Code

--- backend/src/section/delete.ts
+++ backend/src/section/delete.ts
@@ -4,43 +4,41 @@ import { Section } from "../models/section";
 import { Class } from "../models/class";
 
 export const deleteSection = (app: Elysia) =>
-  app
-    .use(auth)
-    .delete("/:id", async ({ profile, set, params: { id } }) => {
-      if (set.status !== 200) return "Unauthorized";
-      if (!profile) {
-        set.status = 401;
-        return "Unauthorized";
-      }
+  app.use(auth).delete("/:id", async ({ profile, set, params: { id } }) => {
+    if (set.status !== 200) return "Unauthorized";
+    if (!profile) {
+      set.status = 401;
+      return "Unauthorized";
+    }
 
-      const isClass = await Class.findOneBy({
-        instructorId: Number(profile.id),
-        id: Number(id),
-      });
-      if (!isClass) {
-        set.status = 404;
-        return "Class Not Found";
-      }
+    const isClass = await Class.findOneBy({
+      instructorId: Number(profile.id),
+      id: Number(id),
+    });
+    if (!isClass) {
+      set.status = 404;
+      return "Class Not Found";
+    }
 
-      try {
-        await Section.createQueryBuilder("section")
-          .delete()
-          .from(Section)
-          .where("id = :id", { id: id })
-          .execute();
-      } catch (err) {
-        set.status = 500;
-        console.log(err);
-        return "Query Failed";
-      }
+    try {
+      await Section.createQueryBuilder("section")
+        .delete()
+        .from(Section)
+        .where("id = :id", { id: id })
+        .execute();
+    } catch (err) {
+      set.status = 500;
+      console.log(err);
+      return "Query Failed";
+    }
 
-      return {
-        data: {
-          class: {
-            section: {
-              id: id,
-            },
+    return {
+      data: {
+        class: {
+          section: {
+            id: id,
           },
         },
-      };
-    });
+      },
+    };
+  });

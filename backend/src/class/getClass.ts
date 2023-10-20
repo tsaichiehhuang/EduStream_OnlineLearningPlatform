466cd355549c25e3125b74ad67cfdf807cbd982f
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-10-20 23:53:03 +0800

    feat(homework): show hw submission if student logged in

--- backend/src/class/getClass.ts
+++ backend/src/class/getClass.ts
@@ -3,6 +3,8 @@ import { Elysia, t } from "elysia";
 import { Class } from "../models/class";
 import { auth } from "../utils/auth";
 import { ElysiaRequest } from "../types/typeConverter";
+import { UserRole } from "../types/type";
+import { Submission } from "../models/submission";
 
 export const getClass = (
   app: Elysia<
@@ -35,17 +37,46 @@ export const getClass = (
         .where("class.id = :classId", { classId: params.classId })
         .getOne();
 
-
       if (!result) {
         set.status = 404;
         return { error: "No resources were found with given class ID." };
       } else {
+        let submittedHwIds: number[] | undefined = undefined;
+        if (profile.role === UserRole.student) {
+          submittedHwIds = (
+            await Submission.find({
+              where: result
+                .sections!.map((sec) =>
+                  sec
+                    .blocks!.filter((blk) => blk.homework !== undefined)
+                    .map((blk) => blk.homework!.id)
+                )
+                .flat()
+                .map((hwId) => ({
+                  userId: profile.id,
+                  hwId: hwId,
+                })),
+            })
+          ).map((submission) => submission.hwId);
+        }
         set.status = 200;
         return {
           data: {
             class: {
               id: result.id,
-              sections: result.sections,
+              sections: result.sections!.map((sec) =>
+                sec.blocks!.map((blk) => ({
+                  ...blk,
+                  homework: blk.homework
+                    ? {
+                        ...blk.homework,
+                        done: submittedHwIds?.some(
+                          (subHwId) => subHwId === blk.homework!.id
+                        ),
+                      }
+                    : undefined,
+                }))
+              ),
             },
           },
         };

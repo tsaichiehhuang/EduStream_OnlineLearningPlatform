03d5984c433af8a12d9f360ac904ed67559c3f0c
timsu92 <33785401+timsu92@users.noreply.github.com>
2023-10-21 00:58:30 +0800

    fix(homework): ignore null homework

--- backend/src/class/getClass.ts
+++ backend/src/class/getClass.ts
@@ -48,7 +48,10 @@ export const getClass = (
               where: result
                 .sections!.map((sec) =>
                   sec
-                    .blocks!.filter((blk) => blk.homework !== undefined)
+                    .blocks!.filter(
+                      (blk) =>
+                        blk.homework !== null && blk.homework !== undefined
+                    )
                     .map((blk) => blk.homework!.id)
                 )
                 .flat()

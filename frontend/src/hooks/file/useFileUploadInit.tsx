7cf971b2fec3464838bdf31d7f1010cd7a9f5f47
penny <penny2014928@gmail.com>
2023-10-20 12:41:23 +0800

    feat:upload file on local

---
+++ frontend/src/hooks/file/useFileUploadInit.tsx
@@ -0,0 +1,62 @@
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
+
+const apiUrl = process.env.API_DOMAIN;
+
+function useFileUploadInit() {
+  const router = useRouter();
+  const token = Cookies.get("accessToken");
+  const classid = Cookies.get("classId");
+
+  const fileuploadinit = async (name: string, classid: string) => {
+    const requestBody = {
+      name: name,
+      classID: classid,
+    };
+    try {
+      const response = await fetch(
+        `https://api.one-stage.kkstream.io/bv/cms/v1/lives`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(requestBody),
+        }
+      );
+      const responseData = await response.json();
+      if (response.ok) {
+        Swal.fire({
+          icon: "success",
+          title: "檔案準備上傳成功",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+        
+        if(responseData.location == "local"){
+
+        }else{
+
+        }
+
+        setTimeout(() => {
+          router.push("/");
+          window.location.reload();
+        }, 1000);
+      } else {
+        Swal.fire("檔案準備上傳失敗", "", "warning");
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "網路請求錯誤",
+        text: "請稍後再試或通知我們的工程團隊。",
+      });
+    }
+  };
+
+  return { fileuploadinit };
+}
+
+export default useFileUploadInit;
---
+++ frontend/src/hooks/file/useUploadTeacher.tsx
@@ -0,0 +1,47 @@
+import Cookies from 'js-cookie'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Swal from 'sweetalert2'
+import useFileUploadInit from "@/hooks/file/useFileUploadInit";
+
+const apiUrl = process.env.API_DOMAIN
+
+function useUploadLocalTeacher() {
+    const { fileuploadinit } = useFileUploadInit();
+    const router = useRouter()
+    const { id } = router.query
+    const [defaultInfoData, setDefaultInfoData] = useState<DefaultData[]>([])
+    const accessToken = Cookies.get('accessToken')
+
+   
+
+    const uploadlocalteacher = async (requestbody: any) => {
+        try {
+            const response = await fetch(`${apiUrl}/class/${id}/section`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify(requestbody),
+            })
+            if (response.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: '檔案準備新增成功',
+                    showConfirmButton: false,
+                    timer: 500,
+                })
+                setTimeout(() => {
+                    window.location.reload()
+                }, 1000)
+            }
+        } catch (error) {
+            console.error('Error fetching class data:', error)
+            Swal.fire('檔案準備新增失敗', '', 'warning')
+        }
+    }
+
+    return { uploadlocalteacher }
+}
+
+export default useUploadLocalTeacher

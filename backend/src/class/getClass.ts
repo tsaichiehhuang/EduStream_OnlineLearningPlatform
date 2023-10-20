7d621b620b1a78d44d9333c84e72b8d7f177b754
lai10297000 <lai10297000@users.noreply.github.com>
2023-10-21 00:35:38 +0800

    chore(Prettier): Prettified Code

--- backend/src/class/getClass.ts
+++ backend/src/class/getClass.ts
@@ -34,7 +34,9 @@ export const getClass = (
         .leftJoinAndSelect("blocks.file", "file")
         .leftJoinAndSelect("blocks.announcement", "announcement")
         .leftJoinAndSelect("blocks.homework", "homework")
-        .where("class.id = :classId AND sections.order <> 0 ", { classId: params.classId })
+        .where("class.id = :classId AND sections.order <> 0 ", {
+          classId: params.classId,
+        })
         .getOne();
 
       if (!result) {

b6f44dd9a50802a29d22c26c78ec8014d87aec62
Distant22 <Distant22@users.noreply.github.com>
2023-10-21 16:56:26 +0800

    chore(Prettier): Prettified Code

--- backend/src/live/get.ts
+++ backend/src/live/get.ts
@@ -65,34 +65,35 @@ export const getLive = (app: Elysia) =>
             result.live.setup == null
               ? null
               : result.live.setup.rtmp.links[0].stream_key;
-          const url = result.live.stream[0] == null
-            ? null
-            : result.live.stream[0].manifests[0].uris[0].uri;
+          const url =
+            result.live.stream[0] == null
+              ? null
+              : result.live.stream[0].manifests[0].uris[0].uri;
 
-        return {
-          data: {
-            live: {
-              id: result.live.id,
-              name: result.live.name,
-              created_at: result.live.created_at,
-              updated_at: result.live.updated_at,
-              status: result.live.status,
-              setup: {
-                links: link,
-                key: key,
+          return {
+            data: {
+              live: {
+                id: result.live.id,
+                name: result.live.name,
+                created_at: result.live.created_at,
+                updated_at: result.live.updated_at,
+                status: result.live.status,
+                setup: {
+                  links: link,
+                  key: key,
+                },
+                url: url,
               },
-              url: url,
             },
-          },
-        };
-      }
-    },
-    {
-      params: t.Object({
-        liveId: t.String({
-          format: "uuid",
-          default: "00000000-0000-0000-0000-000000000000",
+          };
+        }
+      },
+      {
+        params: t.Object({
+          liveId: t.String({
+            format: "uuid",
+            default: "00000000-0000-0000-0000-000000000000",
+          }),
         }),
-      }),
-    }
-  );
+      }
+    );

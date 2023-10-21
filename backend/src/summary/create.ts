9c2d96f5940aa8ded1a482e72ef5b84105955744
Distant22 <sindistant@gmail.com>
2023-10-22 01:13:37 +0800

    fix(summary):create summary API

--- backend/src/summary/create.ts
+++ backend/src/summary/create.ts
@@ -1,17 +1,14 @@
-// path.resolve(staticRoot, dbFile.path)
-// file/download.ts
-
-// const dbFile = await File.findOneBy({ id: fileId });
-// import { KK_API_ENDPOINT, staticRoot } from "../utils/constant";
-
 import { Elysia, t } from "elysia";
 import { File } from "../models/file"
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { KK_API_ENDPOINT, staticRoot } from "../utils/constant";
+import { staticRoot } from "../utils/constant";
 import path from "path";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { Chroma } from "langchain/vectorstores/chroma";
 import { CharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAI } from "langchain/llms/openai";
-import { loadSummarizationChain } from "langchain/chains";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
 export const createSummary = (app: Elysia) =>
   app.post(
@@ -41,17 +38,23 @@ export const createSummary = (app: Elysia) =>
                 chunkOverlap: Math.floor(concat_doc_len/5) == 0 ? 1 : Math.floor(concat_doc_len/5),
             });
             const output = await splitter.createDocuments([concat_doc]);
-            console.warn(output, concat_doc_len,  Math.floor(concat_doc_len/5), output.length )
 
-            const chain = loadSummarizationChain(new OpenAI({ temperature: 0 }), { type: "map_reduce" });
-            const answer = await chain.call({
-            input_documents: docs,
+            const vectorStore = await MemoryVectorStore.fromDocuments(
+                output,
+                new OpenAIEmbeddings()
+            );
+
+            const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
+            const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+
+            const response = await chain.call({
+                query: "這份文件的摘要是什麼？",
             });
-            console.warn({ answer });
+            console.warn(response);
 
             set.status = 200
             return {
-                message: answer
+                message: response
             }
 
         } catch (err) {
